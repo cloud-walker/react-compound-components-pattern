fbcd360512131684952c99b2caa5c4260f4ff788
Luca Barone <baro.luc@gmail.com>
2017-08-12 18:14:37 +0200

    first working draft

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,64 @@
 import React from 'react'
+import styled from 'styled-components'
+import R from 'ramda'
 
 import Tabs from 'components/Tabs'
+
+const ifActive = R.curry((a, b) =>
+  R.ifElse(R.prop('isActive'), R.always(a), R.always(b)),
+)
+
+const TabList = styled.div`display: flex;`
+
+const TabPanel = styled.div`
+  border: 1px solid green;
+  padding: 1rem;
+
+  display: ${ifActive('block')('none')};
+`
+
+const Tab = styled.div`
+  cursor: pointer;
+  text-align: center;
+  padding: .5rem;
+  border-top: 1px solid;
+  border-left: 1px solid;
+  border-right: 1px solid;
+
+  background-color: ${ifActive('red', 'white')};
+`
+
+const FluffyKin = styled.div`
+  display: inline-block;
+  padding: .5rem;
+  border-radius: 50%;
+  font-weight: ${ifActive('bold', 'normal')};
+  background-color: #333;
+  color: #eee;
+  cursor: pointer;
+`
+
+const VerticalTabsLayout = styled.div`display: flex;`
+
+const VerticalTabList = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const VerticalTab = styled.div`
+  padding: .5rem;
+  border: 1px solid;
+  cursor: pointer;
+  font-weight: ${ifActive('bold', 'normal')};
+`
+
+const VerticalLayoutPanel = styled.div`
+  color: green;
+  border: 1px solid;
+  padding: 1rem;
+  flex-grow: 1;
+  display: ${ifActive('block', 'none')};
+`
 
 class App extends React.Component {
   static displayName = 'App'
@@ -8,7 +66,58 @@ class App extends React.Component {
   render() {
     return (
       <main>
-        <Tabs />
+        <Tabs>
+          {({activeIndex, getTabProps, getPanelProps}) => {
+            const pizzaTabProps = getTabProps()
+            const sushiTabProps = getTabProps()
+            const pastaTabProps = getTabProps()
+
+            const pizzaPanelProps = getPanelProps()
+            const sushiPanelProps = getPanelProps()
+            const pastaPanelProps = getPanelProps()
+
+            return (
+              <div>
+                <TabList>
+                  <Tab {...pizzaTabProps}>pizza</Tab>
+                  <Tab {...sushiTabProps}>sushi</Tab>
+                  <Tab {...pastaTabProps}>pasta</Tab>
+                </TabList>
+
+                <div>
+                  <TabPanel {...pizzaPanelProps}>
+                    Pizza is the best food evah.
+                    <FluffyKin {...sushiTabProps}>sushi</FluffyKin>
+                  </TabPanel>
+                  <TabPanel {...sushiPanelProps}>
+                    Good sushi can be good almost as pizza!
+                  </TabPanel>
+                  <TabPanel {...pastaPanelProps}>Pasta is... pasta!</TabPanel>
+                </div>
+
+                <VerticalTabsLayout>
+                  <VerticalTabList>
+                    <VerticalTab {...pizzaTabProps}>pizza</VerticalTab>
+                    <VerticalTab {...sushiTabProps}>sushi</VerticalTab>
+                    <VerticalTab {...pastaTabProps}>pasta</VerticalTab>
+                  </VerticalTabList>
+
+                  <VerticalLayoutPanel {...pizzaPanelProps}>
+                    Pizza is the best food evah.
+                  </VerticalLayoutPanel>
+
+                  <VerticalLayoutPanel {...sushiPanelProps}>
+                    Good sushi can be good almost as pizza!
+                  </VerticalLayoutPanel>
+
+                  <VerticalLayoutPanel {...pastaPanelProps}>
+                    Pasta is... pasta!
+                  </VerticalLayoutPanel>
+                </VerticalTabsLayout>
+              </div>
+            )
+          }}
+        </Tabs>
       </main>
     )
   }
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,9 +1,80 @@
 import React from 'react'
 
-const Component = () => (
-  <div>...</div>
-)
+const Component = class extends React.Component {
+  static displayName = 'Tabs'
 
-Component.displayName = 'Tabs'
+  get isGetterPropsUseValid() {
+    const tabs = this.tabs
+    const panels = this.panels
+
+    return tabs === panels && tabs + panels >= 4
+  }
+
+  constructor(...args) {
+    super(...args)
+    this.getTabProps = this.getTabProps.bind(this)
+    this.getPanelProps = this.getPanelProps.bind(this)
+    this.handleChangeTab = this.handleChangeTab.bind(this)
+
+    this.tabs = -1
+    this.panels = -1
+
+    this.state = {
+      activeIndex: this.props.index || 0,
+    }
+  }
+
+  handleChangeTab(activeIndex) {
+    return () => this.setState({activeIndex})
+  }
+
+  getTabProps(props) {
+    this.tabs++
+    const index = this.tabs
+
+    return {
+      ...props,
+      onClick: this.handleChangeTab(index),
+      isActive: this.state.activeIndex === index,
+    }
+  }
+
+  getPanelProps(props) {
+    this.panels++
+    const index = this.panels
+
+    return {
+      ...props,
+      isActive: this.state.activeIndex === index,
+    }
+  }
+
+  componentWillUpdate() {
+    this.panels = -1
+    this.tabs = -1
+  }
+
+  render() {
+    const {activeIndex} = this.state
+    const {getPanelProps, getTabProps} = this
+
+    const result = this.props.children({
+      activeIndex,
+      getTabProps,
+      getPanelProps,
+    })
+
+    if (!this.isGetterPropsUseValid) {
+      console.warn(`
+        Tabs and/or panels are not sufficient or
+        not the same number!
+        Please use the getterProps provided to add
+        at least 2 panels and 2 tabs.
+        `)
+    }
+
+    return result
+  }
+}
 
 export default Component
